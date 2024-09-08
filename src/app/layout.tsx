b949cec182aba13a691165db1824fbc913e62a4b
Ericsen Tsai <bommer95175@gmail.com>
2024-09-08 15:14:33 +0800

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,34 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Ericsen - A Frontend Developer",
   description: "Ericsen • Frontend Developer • Gardener • Freediver",
-  keywords: ["Ericsen", "Frontend Developer", "Freediver"],
+  keywords: ["Ericsen", "Frontend Developer", "Typescript", "Nextjs"],
   creator: "ericsen-tsai",
+  manifest: "/favicon/site.webmanifest",
+  icons: {
+    icon: "/favicon/favicon.ico",
+    shortcut: "/favicon/favicon.ico",
+    apple: [
+      {
+        url: "/favicon/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    other: [
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/favicon/favicon-16x16.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/favicon/favicon-32x32.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
