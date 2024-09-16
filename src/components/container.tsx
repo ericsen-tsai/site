4fa603a8dd3f806fb303917927382996e40e0b2e
Ericsen Tsai <bommer95175@gmail.com>
2024-09-16 21:41:02 +0800

    feat: add tooltip with image

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,8 @@
 function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto min-h-screen max-w-4xl px-8 py-10">{children}</div>
+    <div className="mx-auto min-h-screen max-w-4xl px-8 pb-10 pt-24">
+      {children}
+    </div>
   );
 }
 
--- src/components/photo-gallery.tsx
+++ src/components/photo-gallery.tsx
@@ -1,55 +1,81 @@
 "use client";
 
 import Image from "next/image";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 
-const photos = [
+const PHOTOS = [
   {
     src: "/photo-gallery/fire_stick.png",
     alt: "Fire Stick",
     spanRow: 2,
     spanCol: 1,
+    description: "Fire stick practice during college",
   },
-  { src: "/photo-gallery/camping.png", alt: "Camping", spanRow: 3, spanCol: 2 },
+  {
+    src: "/photo-gallery/camping.png",
+    alt: "Camping",
+    spanRow: 3,
+    spanCol: 2,
+    description: "Camping with Dollars and Sense",
+  },
   {
     src: "/photo-gallery/guinea_pigs.png",
     alt: "Guinea Pigs",
     spanRow: 1,
     spanCol: 1,
+    description:
+      "My guinea pigs, Fengui and Mixiang (which means rice cake and puffed rice respectively in Chinese)",
   },
   {
     src: "/photo-gallery/freediving.png",
     alt: "Freediving",
     spanRow: 2,
     spanCol: 2,
+    description: "Freediving at Plane Wreck, Bohol with Justina",
   },
   {
     src: "/photo-gallery/guinea_pigs.png",
     alt: "Guinea Pigs",
     spanRow: 2,
     spanCol: 1,
+    description:
+      "My guinea pigs, Fengui and Mixiang (which means rice cake and puffed rice respectively in Chinese)",
   },
 ];
 
 function PhotoGallery() {
   return (
     <div className="grid auto-rows-[100px] grid-cols-3 gap-4">
-      {photos.map((photo, index) => (
-        <div
-          key={index}
-          className="relative overflow-hidden rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:scale-105"
-          style={{
-            gridRow: `span ${photo.spanRow}`,
-            gridColumn: `span ${photo.spanCol}`,
-          }}
-        >
-          <Image
-            src={photo.src}
-            alt={photo.alt}
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="object-cover transition-opacity duration-300 ease-in-out hover:opacity-80"
-          />
-        </div>
+      {PHOTOS.map((photo, index) => (
+        <TooltipProvider key={index} delayDuration={200}>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div
+                className="relative overflow-hidden rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:scale-105"
+                style={{
+                  gridRow: `span ${photo.spanRow}`,
+                  gridColumn: `span ${photo.spanCol}`,
+                }}
+              >
+                <Image
+                  src={photo.src}
+                  alt={photo.alt}
+                  fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  className="object-cover transition-opacity duration-300 ease-in-out hover:opacity-80"
+                />
+              </div>
+            </TooltipTrigger>
+            <TooltipContent asChild className="bg-secondary">
+              <p className="max-w-xs text-sm">{photo.description}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       ))}
     </div>
   );
