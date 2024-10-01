33ce67a5058c67785efc22eddfbe08005b069d33
Ericsen Tsai <bommer95175@gmail.com>
2024-10-01 22:22:32 +0800

    build: migrate repo to turbo

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -6,7 +6,7 @@ loadEnvConfig(projectDir);
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
-  out: "./migrations",
+  out: "./src/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.POSTGRES_URL!,
