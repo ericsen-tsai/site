3508776f8e918b5744182b531ad68c1bab42fded
Ericsen Tsai <bommer95175@gmail.com>
2025-05-05 13:09:14 +0800

    feat: increase max file size to 8MB

--- apps/cds-editor/src/app/api/uploadthing/core.ts
+++ apps/cds-editor/src/app/api/uploadthing/core.ts
@@ -5,7 +5,7 @@ const f = createUploadthing();
 export const ourFileRouter = {
   imageUploader: f({
     image: {
-      maxFileSize: "4MB",
+      maxFileSize: "8MB",
       maxFileCount: 1
     }
   }).onUploadComplete(({ file }) => {
